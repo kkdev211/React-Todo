6c8dc12f9f74c4952389bb974effb0fcb8fb74f6
kkdev211 <kkdev211@users.noreply.github.com>
2020-03-11 21:25:43 -0400

    set up app.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,38 @@
 import React from 'react';
+import ReactDOM from "react-dom";
+
+const tasks =[
+  {
+    task: 'Organize Garage',
+    id: 1528817077286,
+    completed: false
+  },
+  {
+    task: 'Bake Cookies',
+    id: 1528817084358,
+    completed: false
+  }
+];
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      tasks
+    };
+  }
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
   render() {
     return (
-      <div>
-        <h2>Welcome to your Todo App!</h2>
+      <div className="App">
+        <div className="header">
+          <h1> To Do List</h1>
+        </div>
       </div>
     );
   }
 }
-
-export default App;
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
